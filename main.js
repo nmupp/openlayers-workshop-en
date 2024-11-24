58366ec1c935e227a06b605482501d941c9ef2b8
Narasimhulu Muppalla <nmupphere@gmail.com>
2024-11-23 23:14:44 -0600

    add draw interaction

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import DragAndDrop from 'ol/interaction/DragAndDrop';
 import GeoJSON from 'ol/format/GeoJSON';
 import Link from 'ol/interaction/Link';
 import Map from 'ol/Map';
+import Modify from 'ol/interaction/Modify';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+import Draw from 'ol/interaction/Draw';
 import View from 'ol/View';
 import {fromLonLat} from 'ol/proj';
 
@@ -34,5 +36,11 @@ map.addInteraction(
   new DragAndDrop({
     source,
     formatConstructors: [GeoJSON],
+  })
+);
+map.addInteraction(
+  new Draw({
+    type: 'Polygon',
+    source,
   })
 );
