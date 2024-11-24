f2569a8226e4a50bec7b07df589b19b6c4e98b78
Narasimhulu Muppalla <nmupphere@gmail.com>
2024-11-23 22:46:21 -0600

    base map with interactions and drag drop

---
+++ main.js
@@ -0,0 +1,38 @@
+import DragAndDrop from 'ol/interaction/DragAndDrop';
+import GeoJSON from 'ol/format/GeoJSON';
+import Link from 'ol/interaction/Link';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import {fromLonLat} from 'ol/proj';
+
+const map = new Map({
+  target: 'map-container',
+  layers: [
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: './data/countries.json',
+      }),
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([0, 0]),
+    zoom: 2,
+  }),
+});
+
+map.addInteraction(new Link());
+
+const source = new VectorSource();
+const layer = new VectorLayer({
+  source: source,
+});
+map.addLayer(layer);
+map.addInteraction(
+  new DragAndDrop({
+    source,
+    formatConstructors: [GeoJSON],
+  })
+);
