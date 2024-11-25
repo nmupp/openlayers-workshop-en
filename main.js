54a41820434dad0610b13df90a48760580b8fe44
Narasimhulu Muppalla <nmupphere@gmail.com>
2024-11-25 06:07:07 -0600

    geo location

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ import View from 'ol/View';
 import {fromLonLat} from 'ol/proj';
 import colormap from 'colormap';
 import {getArea} from 'ol/sphere';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import {circular} from 'ol/geom/Polygon';
+import Control from 'ol/control/Control';
 
 const min = 1e8; // the smallest area
 const max = 2e13; // the biggest area
@@ -97,3 +101,40 @@ source.on('change', function () {
   download.href =
     'data:application/json;charset=utf-8,' + encodeURIComponent(json);
 });
+
+navigator.geolocation.watchPosition(
+    function (pos) {
+      const coords = [pos.coords.longitude, pos.coords.latitude];
+      const accuracy = circular(coords, pos.coords.accuracy);
+      source.clear(true);
+      source.addFeatures([
+        new Feature(
+          accuracy.transform('EPSG:4326', map.getView().getProjection())
+        ),
+        new Feature(new Point(fromLonLat(coords))),
+      ]);
+    },
+    function (error) {
+      alert(`ERROR: ${error.message}`);
+    },
+    {
+      enableHighAccuracy: true,
+    }
+  );
+
+const locate = document.createElement('div');
+locate.className = 'ol-control ol-unselectable locate';
+locate.innerHTML = '<button title="Locate me">◎</button>';
+locate.addEventListener('click', function () {
+if (!source.isEmpty()) {
+    map.getView().fit(source.getExtent(), {
+    maxZoom: 18,
+    duration: 500,
+    });
+}
+});
+map.addControl(
+new Control({
+    element: locate,
+})
+);
